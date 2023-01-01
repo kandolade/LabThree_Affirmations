fd6599494fb0e6ceccf44518035455d6460ab9d4
kandolade <211223@astanait.edu.kz>
2023-01-01 23:58:20 +0600

    Initial commit

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,6 @@
+package com.example.affirmations
+
+import android.content.Context
+
+
+
---
+++ app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.affirmations.MainActivity
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test() {
+
+
+    }
+}
